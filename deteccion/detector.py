a4c39b2a2877a7058a930d9789435d6a9ee4187c
DANNER5472 <jamanca@gmail.com>
2026-03-07 22:04:19 -0400

    main.py

---
+++ deteccion/detector.py
@@ -0,0 +1,46 @@
+from ultralytics import YOLO
+import cv2
+#Funcion para detectar vehiculos y personas
+class Carril:
+    def __init__(self,ruta_modelo="yolov8n.pt"):
+        self.modelo = YOLO(ruta_modelo)
+        self.categorias_autos = ["car", "truck", "bus", "motorcycle"]
+        self.categorias_peatones = ["person"]
+        
+#Funcion para que YOLO analice el video
+    def analizar_video(self, ruta_video):
+        video = cv2.VideoCapture(ruta_video)
+
+        conteos_autos=[]
+        conteos_peatones =[]
+
+#Aqui analiza todo el video por frames
+        print(f"Analizando video: {ruta_video}")
+
+        while True:
+            hay_frame,frame = video.read()
+            if not hay_frame:
+                break
+            resultados = self.modelo(frame, verbose=False)
+
+            autos_en_frame    =0
+            peatones_en_frame =0
+            for objeto in resultados[0].boxes:
+                categoria =self.modelo.names[int(objeto.cls)]
+
+                if categoria in self.categorias_autos:
+                        autos_en_frame +=1
+
+                if categoria in self.categorias_peatones:
+                        peatones_en_frame +=1
+
+            conteos_autos.append(autos_en_frame)
+            conteos_peatones.append(peatones_en_frame)
+
+ # Se cierra el video
+        video.release()
+
+#Conteo de el maximo de autos y peatoness
+        maximo_autos= max(conteos_autos)
+        maximo_peatones =max(conteos_peatones)       
+        return maximo_autos, maximo_peatones
