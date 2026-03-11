79342a601d4eff84ca04c2f8408b50c2df43e0a8
DANNER5472 <jamanca@gmail.com>
2026-03-10 22:56:24 -0400

    arreglitos en el main

--- main.py
+++ main.py
@@ -2,16 +2,14 @@
 from configuracion import CARRILES, VIDEOS
 from deteccion.detector import Carril
 
-
 def main():
-    print("\n" + "="*55)
+    print("\n"+"="*55)
     print("SISTEMA DE SEMÁFORO INTELIGENTE - PID + YOLO")
     print("="*55)
 
 #Inicializo el detector con el modelo de YOLO
     detector = Carril(ruta_modelo="yolov8n.pt")
-
-# Proceso cada carril uno por uno
+#carril uno por uno
     for nombre_carril in CARRILES:
         print(f"\n{'─'*55}")
         print(f" Procesando carril:{nombre_carril.upper()}")
@@ -22,13 +20,13 @@ def main():
         print("YOLO")
         maximo_vehiculos, maximo_peatones = detector.analizar_video(ruta_video)
 
-        print(f"Vehículos detectados: {maximo_vehiculos}")
-        print(f"Peatones detectados:  {maximo_peatones}")
+        print(f"Vehículos detectados:{maximo_vehiculos}")
+        print(f"Peatones detectados:{maximo_peatones}")
 # meustra resultados
-        print(f"Controlador PID: pendiente")
-        print(f"Simulación semáforo: pendiente")
+        print(f"Controlador PID:pendiente")
+        print(f"Simulación semáforo:pendiente")
 
-    print("\n  Detección finalizada.\n")
+    print("\nDetección finalizada\n")
 
 if __name__ == "__main__":
     main()
